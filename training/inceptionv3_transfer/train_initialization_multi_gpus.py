09f44a532dc2fb6a5a4e7c68ec6cd484e3fdd173
dupei <dupei.dp@antfin.com>
2019-04-28 19:49:33 +0800

    add resnet50

--- training/inceptionv3_transfer/train_initialization_multi_gpus.py
+++ training/inceptionv3_transfer/train_initialization_multi_gpus.py
@@ -6,7 +6,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.backend import clear_session
 from keras.optimizers import SGD
 from pathlib import Path
-from keras.applications import InceptionV3, InceptionResNetV2
+from keras.applications import InceptionV3, InceptionResNetV2, ResNet50
 from keras.models import Sequential, Model, load_model
 from keras.layers import Dense, Dropout, Flatten, AveragePooling2D
 from keras import initializers, regularizers
@@ -26,21 +26,23 @@ NUM_CPU = multiprocessing.cpu_count()
 def build_model(weights_file, type='inception_v3', shape=(299,299,3), nb_output=5):
     print('shape:', shape)
     if type == 'inception_v3':
-        conv_base = InceptionV3(weights='imagenet', include_top=False, input_shape=shape)
+        basic_model = InceptionV3(weights='imagenet', include_top=False, input_shape=shape)
     elif type == 'inception_resnet_v2':
-        conv_base = InceptionResNetV2(weights='imagenet', include_top=False, input_shape=shape)
+        basic_model = InceptionResNetV2(weights='imagenet', include_top=False, input_shape=shape)
+    elif type == 'resnet50':
+        basic_model = ResNet50(weights='imagenet', include_top=False, input_shape=shape)
     else:
         raise Exception('Unsupported Model Type!')
 
     # First time run, no unlocking
-    conv_base.trainable = False
+    basic_model.trainable = False
 
     # Let's see it
     print('Summary')
-    print(conv_base.summary())
+    print(basic_model.summary())
 
     # Let's construct that top layer replacement
-    x = conv_base.output
+    x = basic_model.output
     x = AveragePooling2D(pool_size=(8, 8))(x)
     x - Dropout(0.4)(x)
     x = Flatten()(x)
@@ -52,7 +54,7 @@ def build_model(weights_file, type='inception_v3', shape=(299,299,3), nb_output=
     predictions = Dense(nb_output,  kernel_initializer="glorot_uniform", activation='softmax')(x)
 
     print('Stacking New Layers')
-    model = Model(inputs = conv_base.input, outputs=predictions)
+    model = Model(inputs = basic_model.input, outputs=predictions)
 
     # Load checkpoint if one is found
     if os.path.exists(weights_file):
@@ -84,6 +86,11 @@ def train_model(model_type, weights_file, image_dir, nb_gpu):
         
         # make the model parallel
         model = multi_gpu_model(model, gpus=nb_gpu)
+
+        x = model.output
+        x = Flatten()(x)
+        predictions = Dense(nb_output, activation='softmax', name='new_outputs')(x)
+        model = Model(inputs = model.input, outputs=predictions)
 
     if not os.path.exists(weights_file):
         weights_file = "weights.{}.{}.gpu{}.hdf5".format(model_type, height, nb_gpu)
@@ -156,6 +163,6 @@ if __name__ == "__main__":
     parser.add_argument("-g", "--gpus", type=int, default=1, help="Number of GPUs")
     parser.add_argument("-w", "--weights_file", type=str, default='', help="Path to weights_file")
     parser.add_argument("-t", "--type", type=str, default='inception_v3', \
-        help="Model type, inception_v3|inception_resnet_v2")
+        help="Model type, inception_v3|inception_resnet_v2|resnet50|...")
     args = parser.parse_args()
     train_model(args.type, args.weights_file, args.image_dir, args.gpus)
