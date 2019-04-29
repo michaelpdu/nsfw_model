ac899de55472dafcbe26666315a0282b13886608
dupei <dupei.dp@gpus.arcslab.com>
2019-04-29 15:32:24 +0800

    add gpu_index, batch_size, and total_epochs in options

--- training/inceptionv3_transfer/generators.py
+++ training/inceptionv3_transfer/generators.py
@@ -19,7 +19,7 @@ validation_datagen = ImageDataGenerator(
     rescale=1./255
 )
 
-def create_generators(height, width, image_dir=constants.BASE_DIR, nb_gpu=1):
+def create_generators(height, width, image_dir=constants.BASE_DIR, batch_s=constants.GENERATOR_BATCH_SIZE, nb_gpu=1):
     print('Base dir:', image_dir)
     train_dir = os.path.join(image_dir, 'train')
     test_dir = os.path.join(image_dir, 'test')
@@ -28,14 +28,14 @@ def create_generators(height, width, image_dir=constants.BASE_DIR, nb_gpu=1):
         train_dir,
         target_size=(height, width),
         class_mode='categorical',
-        batch_size=constants.GENERATOR_BATCH_SIZE * nb_gpu
+        batch_size=batch_s*nb_gpu
     )
 
     validation_generator = validation_datagen.flow_from_directory(
         test_dir,
         target_size=(height, width),
         class_mode='categorical',
-        batch_size=constants.GENERATOR_BATCH_SIZE * nb_gpu
+        batch_size=batch_s*nb_gpu
     )
 
     return[train_generator, validation_generator]
